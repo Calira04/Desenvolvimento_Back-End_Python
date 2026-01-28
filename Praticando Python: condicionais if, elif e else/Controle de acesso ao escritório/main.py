6c67cbd697c25ec2fadc731dcd94186ed8131db8
Calira04 <carloseaslira@gmail.com>
2026-01-27 21:22:59 -0300

    Controle de acesso ao escritório

---
+++ Praticando Python: condicionais if, elif e else/Controle de acesso ao escritório/main.py
@@ -0,0 +1,6 @@
+hora_atual = int(input('Digite a hora atual (formato 24 horas): '))
+
+if 8 <=hora_atual < 18:
+    print('Acesso permitido. Bem-vindo ao escritório!')
+else:
+    print('Acesso negado. O escritório está fechado.') 
