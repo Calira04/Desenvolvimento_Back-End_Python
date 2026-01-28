3aaa1638f3b29e712d88ba73612850bb6b4f6625
Calira04 <carloseaslira@gmail.com>
2026-01-27 22:17:32 -0300

    Controle de estoque

---
+++ Praticando Python: laços for e while/Controle de estoque/main.py
@@ -0,0 +1,6 @@
+quantidade_estoque = 5
+
+while quantidade_estoque > 0:
+    print(f'Produto em estoque. Quantidade restante: {quantidade_estoque}')
+    quantidade_estoque -= 1
+print('Estoque esgotado!')
